5404fc8b002baa0d9487205c6405cb19c8acd24f
Marshall Banks <marshalljoelbanks@gmail.com>
2024-10-16 10:36:24 -0700

    Added modal functionality for testing. Removed previous success message.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,27 +1,42 @@
 document.getElementById("contact-form").addEventListener("submit", function(event){
     event.preventDefault(); // Prevent form from redirecting
 
-    var form = event.target;
-    var formStatus = document.getElementById("form-status"); // This is the thank-you message
-    
-    // Create FormData object
+    var form = event.target;    
     var formData = new FormData(form);
     var formspreeURL = "https://formspree.io/f/manyydrp";
 
+    // Modal elements
+    var modal = document.getElementById("success-modal");
+    var closeButton = document.querySelector(".close-button")
+
     // Send the form data using fetch API
     fetch(formspreeURL, {
-        method: "POST", // Corrected this line to use quotes around POST
+        method: "POST", 
         body: formData,
         headers: { 'Accept': 'application/json' }
     }).then(function(response) {
         if (response.ok) {
-            formStatus.style.display = "block"; // Show thank you message
+            // Show the modal
+            modal.style.display = "flex"; // Make modal visible
             form.reset(); // Reset the form
         } else {
             alert("Oops! There was a problem submitting your form.");
         }
     }).catch(function(error) {
+        //console.error("Error details:", error);
         alert("There was a problem submitting your form. Please try again.");
     });
+
+    // Close the modal when clicking the "X"
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none"; // Hide the modal
+    });
+
+    // Close the modal when clicking outside of the modal content
+    window.addEventListener("click", function() {
+        if (event.target === modal) {
+            modal.style.display = "none"; // Hide the modal
+        }
+    })
 });
 
