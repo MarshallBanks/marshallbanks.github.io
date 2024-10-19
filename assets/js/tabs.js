ef142fc1867ea9f95c1ac8d335d3d4003f90d7c7
Marshall Banks <marshalljoelbanks@gmail.com>
2024-10-19 15:21:55 -0700

    Updated tabs logic and styling for mobile.

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -7,27 +7,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return window.innerWidth <= 736; // Adjust based on your breakpoint
     }
 
-    if(!isMobile()) {
-        tabs.forEach((tab, index) => {  // index is captured here
-            tab.addEventListener('click', () => {
-                // Remove 'active' class from all tabs
-                tabs.forEach(item => item.classList.remove('active'));
-    
-                // Add 'active' class to the clicked tab
-                tab.classList.add('active');
-    
-                // Remove 'active' class from all tab contents
-                document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
-    
-                // Add 'active' class to the corresponding tab content
-                const activeContent = document.getElementById(tab.getAttribute('data-tab'));
-                activeContent.classList.add('active');
-    
+    tabs.forEach((tab, index) => {  // index is captured here
+        tab.addEventListener('click', () => {
+            // Remove 'active' class from all tabs
+            tabs.forEach(item => item.classList.remove('active'));
+
+            // Add 'active' class to the clicked tab
+            tab.classList.add('active');
+
+            // Remove 'active' class from all tab contents
+            document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+
+            // Add 'active' class to the corresponding tab content
+            const activeContent = document.getElementById(tab.getAttribute('data-tab'));
+            activeContent.classList.add('active');
+
+            if(!isMobile()) {
                 // Apply border radius only if it's not the first tab
                 if (index === 0) {
                     activeContent.style.borderTopLeftRadius = '0px'; // No radius for first tab
                 }
-            });
+            }
         });
-    }
+    });
+
+    
 });
