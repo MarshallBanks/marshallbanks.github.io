ea8212fa22e3130375f9eef0a7818188f67e504a
Marshall Banks <marshalljoelbanks@gmail.com>
2024-10-19 14:29:02 -0700

    Added Resume Button to footer. Updated about me to be mobile friendly for projects and where I've worked.

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -2,25 +2,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const tabs = document.querySelectorAll('.tabs > ul > li');
 
-    tabs.forEach((tab, index) => {  // index is captured here
-        tab.addEventListener('click', () => {
-            // Remove 'active' class from all tabs
-            tabs.forEach(item => item.classList.remove('active'));
+    // Function to check if the screen size is mobile
+    function isMobile() {
+        return window.innerWidth <= 736; // Adjust based on your breakpoint
+    }
 
-            // Add 'active' class to the clicked tab
-            tab.classList.add('active');
-
-            // Remove 'active' class from all tab contents
-            document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
-
-            // Add 'active' class to the corresponding tab content
-            const activeContent = document.getElementById(tab.getAttribute('data-tab'));
-            activeContent.classList.add('active');
-
-            // Apply border radius only if it's not the first tab
-            if (index === 0) {
-                activeContent.style.borderTopLeftRadius = '0px'; // No radius for first tab
-            }
+    if(!isMobile()) {
+        tabs.forEach((tab, index) => {  // index is captured here
+            tab.addEventListener('click', () => {
+                // Remove 'active' class from all tabs
+                tabs.forEach(item => item.classList.remove('active'));
+    
+                // Add 'active' class to the clicked tab
+                tab.classList.add('active');
+    
+                // Remove 'active' class from all tab contents
+                document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+    
+                // Add 'active' class to the corresponding tab content
+                const activeContent = document.getElementById(tab.getAttribute('data-tab'));
+                activeContent.classList.add('active');
+    
+                // Apply border radius only if it's not the first tab
+                if (index === 0) {
+                    activeContent.style.borderTopLeftRadius = '0px'; // No radius for first tab
+                }
+            });
         });
-    });
+    }
 });
